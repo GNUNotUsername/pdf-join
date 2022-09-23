74ed4127998f5af50439795e71b48e0f732ea5bb
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2022-09-23 11:35:39 +1000

    Monolithic commit
    100% of functionality lolol

---
+++ pdf-join.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+TMP=($(echo $RANDOM | md5sum));
+TEMPDIR=$PWD/$TMP;
+mkdir $TEMPDIR;
+FAIL=0;
+for FILE in $@
+do
+	cp $FILE $TEMPDIR 2> /dev/null;
+	FAIL=$?;
+	if [ $FAIL != 0 ] ; then
+		cp $FILE.pdf $TEMPDIR 2> /dev/null;
+		FAIL=$?;
+		if [ $FAIL != 0 ]; then
+			break
+		fi
+	fi
+done
+if [ $FAIL == 0 ]; then
+	./template $TEMPDIR $TEMPDIR/*.pdf;
+	cd $TEMPDIR;
+	pdflatex template.tex > /dev/null;
+	cp template.pdf ../joined.pdf;
+	cd ..;
+fi
+rm -rf $TEMPDIR;
