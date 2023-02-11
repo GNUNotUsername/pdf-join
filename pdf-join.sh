503f3b1a32506b57d6060285b2ede45a6c492150
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-02-11 15:20:57 +1000

    Fixed indentation conformity

--- pdf-join.sh
+++ pdf-join.sh
@@ -5,21 +5,21 @@ mkdir $TEMPDIR;
 FAIL=0;
 for FILE in $@
 do
-	cp $FILE $TEMPDIR 2> /dev/null;
-	FAIL=$?;
-	if [ $FAIL != 0 ] ; then
-		cp $FILE.pdf $TEMPDIR 2> /dev/null;
-		FAIL=$?;
-		if [ $FAIL != 0 ]; then
-			break
-		fi
-	fi
+    cp $FILE $TEMPDIR 2> /dev/null;
+    FAIL=$?;
+    if [ $FAIL != 0 ] ; then
+        cp $FILE.pdf $TEMPDIR 2> /dev/null;
+        FAIL=$?;
+        if [ $FAIL != 0 ]; then
+            break
+        fi
+    fi
 done
 if [ $FAIL == 0 ]; then
-	./template $TEMPDIR $TEMPDIR/*.pdf;
-	cd $TEMPDIR;
-	pdflatex template.tex > /dev/null;
-	cp template.pdf ../joined.pdf;
-	cd ..;
+    ./template $TEMPDIR $TEMPDIR/*.pdf;
+    cd $TEMPDIR;
+    pdflatex template.tex > /dev/null;
+    cp template.pdf ../joined.pdf;
+    cd ..;
 fi
 rm -rf $TEMPDIR;
